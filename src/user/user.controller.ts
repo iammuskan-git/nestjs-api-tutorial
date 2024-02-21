44f19c2aca1784a54dca91e2081346e025c6450d
iammuskan-git <muskanhamal2000@gmail.com>
2024-02-21 16:07:56 +0545

    Add JWT guard(Custom guard) and update user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
 
-    @UseGuards(AuthGuard('jwt'))  // Guards le Strategy lai check garxa token xa kinai ani valid token xa kinai vanera
+    @UseGuards(JwtGuard)  // Guards le Strategy lai check garxa token xa kinai ani valid token xa kinai vanera
     @Get('me')
     getMe(@Req() req: Request){
         return req.user;
